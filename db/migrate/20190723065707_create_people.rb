c94aa00b9678edf38df2851d591337dfdcfd1bac
boratanrikulu <boratanrikulu@gmail.com>
2019-07-23 10:32:27 +0300

    Day5

---
+++ db/migrate/20190723065707_create_people.rb
@@ -0,0 +1,8 @@
+class CreatePeople < ActiveRecord::Migration[6.0]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_165102) do
+ActiveRecord::Schema.define(version: 2019_07_23_065707) do
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2019_07_22_165102) do
     t.integer "product_id", null: false
     t.index ["customer_id", "product_id"], name: "index_customers_products_on_customer_id_and_product_id"
     t.index ["product_id", "customer_id"], name: "index_customers_products_on_product_id_and_customer_id"
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "products", force: :cascade do |t|
